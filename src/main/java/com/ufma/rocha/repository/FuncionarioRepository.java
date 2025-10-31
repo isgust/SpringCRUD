18b1a152a827ffd4fea180fcef486e6e5016eb15
isgust <rocha.gustavops@gmail.com>
2025-10-31 04:56:55 -0300

    Interface Repository Criada

--- src/main/java/com/ufma/rocha/repository/FuncionarioRepository.java
+++ src/main/java/com/ufma/rocha/repository/FuncionarioRepository.java
@@ -1,4 +1,11 @@
 package com.ufma.rocha.repository;
 
-public class FuncionarioRepository {
+import com.ufma.rocha.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+
 }
