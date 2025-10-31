0b3a00fa2d857d656d608ee2e6c993cb98532795
isgust <rocha.gustavops@gmail.com>
2025-10-31 04:34:59 -0300

    Adicionando a classe model do Funcionário

--- src/main/java/com/ufma/rocha/model/Funcionario.java
+++ src/main/java/com/ufma/rocha/model/Funcionario.java
@@ -1,4 +1,25 @@
 package com.ufma.rocha.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity //Informa que se trata de uma tabela
+@Data //Getters e Setters
+@NoArgsConstructor // Construtor vazio
+@AllArgsConstructor // Contrutor completo
 public class Funcionario {
+
+    @Id //Chave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cargo;
+    private Double salario;
 }
